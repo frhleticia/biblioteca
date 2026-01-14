bace8ef53fb9a72120e1bdd4a0efe5173a9d8818
leticia.silva <frhleticia@gmail.com>
2026-01-14 14:00:07 -0300

    Ajustando request

--- biblioteca/src/test/java/com/db/biblioteca/service/LivroServiceTest.java
+++ biblioteca/src/test/java/com/db/biblioteca/service/LivroServiceTest.java
@@ -1,6 +1,6 @@
 package com.db.biblioteca.service;
 
-import com.db.biblioteca.model.Autor;
+import com.db.biblioteca.dto.LivroRequest;
 import com.db.biblioteca.model.Livro;
 import com.db.biblioteca.repository.AutorRepository;
 import com.db.biblioteca.repository.LivroRepository;
@@ -8,13 +8,10 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
-import java.time.Year;
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class LivroServiceTest {
-    private AutorService autorService;
     private LivroService livroService;
 
     @BeforeEach
@@ -22,14 +19,16 @@ public class LivroServiceTest {
         AutorRepository autorRepository = new AutorRepository();
         LivroRepository livroRepository = new LivroRepository();
 
-        autorService = new AutorService(autorRepository);
+        AutorService autorService = new AutorService(autorRepository);
         livroService = new LivroService(livroRepository, autorService);
     }
 
     @Test
     void deveRetornarLivroQuandoExistente() {
 
-        Livro livro = livroService.criarLivro("Odisseia", "12345678901", LocalDate.of(1996, 1, 14));
+        LivroRequest request = new LivroRequest("Odisseia", "12345678901", LocalDate.of(1996, 1, 14));
+
+        Livro livro = livroService.criarLivro(request);
 
         livroService.buscarLivro(livro.getId());
 
@@ -38,12 +37,14 @@ public class LivroServiceTest {
 
     @Test
     void deveAtualizarLivroQuandoTodosDadosValidos() {
+        LivroRequest request = new LivroRequest("Odisseia", "12345678901", LocalDate.of(1996, 1, 14));
+        Livro livro = livroService.criarLivro(request);
 
-        Livro livro = livroService.criarLivro("Odisseia", "12345678901", LocalDate.of(1996, 1, 14));
+        LivroRequest requestAtt = new LivroRequest("A Odisseia", "12345678901", LocalDate.of(1996, 1, 14));
+        Livro atualizacao = livroService.atualizarLivro(livro.getId(), requestAtt);
 
-        Livro atualizado = livroService.atualizarLivro(livro.getId(), "A Odisseia", "12345678901", LocalDate.of(1996, 1, 14));
-        livroService.buscarLivro(atualizado.getId());
+        livroService.buscarLivro(atualizacao.getId());
 
-        assertEquals("A Odisseia", atualizado.getNome());
+        assertEquals("A Odisseia", atualizacao.getNome());
     }
 }
